d6388cf8ccaa7feddb142ffb93160f20e49d42c8
Mateus Deitos <mate.deitos@hotmail.com>
2020-05-13 10:36:25 -0300

    Async/await

---
+++ JavaScript ES6/Async_Await/src/main.js
@@ -0,0 +1,18 @@
+require("regenerator-runtime/runtime");
+const minhaPromise = () => new Promise((resolve, reject) =>
+    setTimeout(() => {resolve('OK')},2000));
+
+// async function executaPromise(){
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+// }
+
+const executaPromise = async () => {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+}
+
+executaPromise();
---
+++ JavaScript ES6/Async_Await/webpack.config.js
@@ -0,0 +1,23 @@
+//Arquivo de configuração do Webpack
+
+module.exports = {
+    entry: ['@babel/polyfill', './src/main.js'],
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js',
+    },
+    devServer:{
+        contentBase: __dirname + '/public'
+    },
+    module:{
+        rules: [
+            {
+                test: /\.js$/,    //Regex: arquivos que terminam com .js
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    }
+}
