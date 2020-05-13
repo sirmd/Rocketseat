60065a0d14eaca0aabdef5c139b034a4e00cc5ac
Mateus Deitos <mate.deitos@hotmail.com>
2020-05-13 11:17:07 -0300

    Configurando axios

--- JavaScript ES6/Async_Await/src/main.js
+++ JavaScript ES6/Async_Await/src/main.js
@@ -1,18 +1,17 @@
-require("regenerator-runtime/runtime");
-const minhaPromise = () => new Promise((resolve, reject) =>
-    setTimeout(() => {resolve('OK')},2000));
+import axios from "axios";
+import "regenerator-runtime/runtime.js";
 
-// async function executaPromise(){
-//     console.log(await minhaPromise());
-//     console.log(await minhaPromise());
-//     console.log(await minhaPromise());
-//     console.log(await minhaPromise());
-// }
+class Api{
+   static async getUserInfo(username){
+       try {
+           
+        const response = await axios.get(`https://api.github.com/users/${username}`);
 
-const executaPromise = async () => {
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
+        console.log(response);
+       } catch (err) {
+           console.warn('Erro na API');
+       }
+   }
 }
 
-executaPromise();
+Api.getUserInfo('sirmd');
