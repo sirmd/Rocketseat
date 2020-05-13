32c2de5acfbe13fc7d4f358e2ecf83dd74b6a3da
Mateus Deitos <mate.deitos@hotmail.com>
2020-05-13 14:18:33 -0300

    JSES6 - Async/Await Desafio 4

--- JavaScript ES6/Async_Await/src/main.js
+++ JavaScript ES6/Async_Await/src/main.js
@@ -1,16 +1,12 @@
 import "regenerator-runtime/runtime.js";
 import axios from 'axios';
 
-
-class Github {
-    static async getRepositories(repo) {
-    try {
-        const response = await axios.get(`https://api.github.com/repos/${repo}`);
-        console.log(response);
-    } catch (err) {
-        console.warn(`Repositório ${repo} não existe`)
-    }
-   }
+const buscaUsuario = async (usuario) => {
+try {
+    const response = await axios.get(`https://api.github.com/users/${usuario}`);
+    console.log(response);
+} catch (error) {
+    console.log(`O usuário ${usuario} não existe`)
 }
-   Github.getRepositories('rocketseat/react-native-template-rocketseat-basic');
-   Github.getRepositories('rocketseat/dslkvmskv');
+}
+buscaUsuario('sirmd');
