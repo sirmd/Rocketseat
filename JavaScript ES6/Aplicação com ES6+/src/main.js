d98e67b75c9ccfe3e6e46843862305764b9b0c47
Mateus Deitos <mate.deitos@hotmail.com>
2020-05-13 17:40:18 -0300

    Aplicação com ES6+ -> Buscando da API

--- JavaScript ES6/Aplicação com ES6+/src/main.js
+++ JavaScript ES6/Aplicação com ES6+/src/main.js
@@ -1,11 +1,12 @@
 import "regenerator-runtime/runtime.js";
-import axios from 'axios';
+import api from "./api";
 
 class App{
     constructor(){
         this.repositories = [];
 
         this.formElement = document.getElementById("repo-form");
+        this.inputElement = document.querySelector('input[name=repository]');
         this.listElement = document.getElementById("repo-list");
 
         this.registerHandlers();
@@ -16,15 +17,29 @@ class App{
        this.formElement.onsubmit = event => this.addRepository(event);
     }
 
-    addRepository(event){
+    async addRepository(event){
         event.preventDefault();
 
+        const repoInput = this.inputElement.value;
+
+        if (repoInput.length === 0) {
+            return;
+        }
+
+        const response = await api.get(`/repos/${repoInput}`);
+        const {name, description, html_url, owner: {avatar_url}} = response.data;
+        
+
+        console.log(name, description, html_url, avatar_url);
+
         this.repositories.push({
-            name: 'rocketseat.com.br',
-            description: 'Tire a sua ideia do papel e dê vida à sua startup',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-            html_url: 'http://github.com/rocketseat',
+            name: name,
+            description: description,
+            avatar_url: avatar_url,
+            html_url: html_url,
         });
+
+        this.inputElement.value = "";
 
         this.render();
     }
@@ -43,7 +58,8 @@ class App{
             descriptionElement.appendChild(document.createTextNode(repo.description));
 
             let likeElement = document.createElement('a');
-            likeElement.setAttribute('target', '_blank');
+            likeElement.setAttribute('target', repo.html_url);
+            likeElement.setAttribute('href', repo.html_url);
             likeElement.appendChild(document.createTextNode('Acessar'));
 
             let listItemElement = document.createElement('li');
