5230356c3b9c893cf2c77f7c042ea84cc0f4b95c
Mateus Deitos <mate.deitos@hotmail.com>
2020-05-13 13:55:27 -0300

    JSES6 - Async/Await Desafio 2

--- JavaScript ES6/Async_Await/src/main.js
+++ JavaScript ES6/Async_Await/src/main.js
@@ -1,34 +1,15 @@
-// import axios from "axios";
-// import "regenerator-runtime/runtime.js";
-
-// class Api{
-//    static async getUserInfo(username){
-//        try {
-           
-//         const response = await axios.get(`https://api.github.com/users/${username}`);
-
-//         console.log(response);
-//        } catch (err) {
-//            console.warn('Erro na API');
-//        }
-//    }
-// }
-
-// Api.getUserInfo('sirmd');
-
 import "regenerator-runtime/runtime.js";
-// Funão delay aciona o .then após 1s
+import axios from 'axios';
 
-const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
-
-const umPorSegundo = async () => {
- 
-    await delay();
-    console.log('1s');
-    await delay();
-    console.log('2s');
-    await delay();
-    console.log('3s');
- 
+async function getUserFromGithub(user) {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${user}`);
+        console.log(response);
+    } catch (err) {
+        console.log(`Usuário ${user} não existe`);
+        
+    }
 }
-umPorSegundo();
+ 
+getUserFromGithub('sirmd');
+getUserFromGithub('diego3g124123');
