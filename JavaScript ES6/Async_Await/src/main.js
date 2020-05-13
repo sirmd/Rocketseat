eb0bd976fed5a2acab437babb0b5964f5a1b2c29
Mateus Deitos <mate.deitos@hotmail.com>
2020-05-13 13:20:43 -0300

    JSES6 - Async/Await Desafio 1

--- JavaScript ES6/Async_Await/src/main.js
+++ JavaScript ES6/Async_Await/src/main.js
@@ -1,17 +1,34 @@
-import axios from "axios";
-import "regenerator-runtime/runtime.js";
+// import axios from "axios";
+// import "regenerator-runtime/runtime.js";
 
-class Api{
-   static async getUserInfo(username){
-       try {
+// class Api{
+//    static async getUserInfo(username){
+//        try {
            
-        const response = await axios.get(`https://api.github.com/users/${username}`);
+//         const response = await axios.get(`https://api.github.com/users/${username}`);
 
-        console.log(response);
-       } catch (err) {
-           console.warn('Erro na API');
-       }
-   }
+//         console.log(response);
+//        } catch (err) {
+//            console.warn('Erro na API');
+//        }
+//    }
+// }
+
+// Api.getUserInfo('sirmd');
+
+import "regenerator-runtime/runtime.js";
+// Funão delay aciona o .then após 1s
+
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+const umPorSegundo = async () => {
+ 
+    await delay();
+    console.log('1s');
+    await delay();
+    console.log('2s');
+    await delay();
+    console.log('3s');
+ 
 }
-
-Api.getUserInfo('sirmd');
+umPorSegundo();
