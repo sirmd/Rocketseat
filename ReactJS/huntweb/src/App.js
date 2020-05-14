beb7ae8fb20ab99d1941d8ac668737e4b8282724
Mateus Deitos <mate.deitos@hotmail.com>
2020-05-14 12:03:03 -0300

    ReactJS -> Buscando produtos da API

--- ReactJS/huntweb/src/App.js
+++ ReactJS/huntweb/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import './styles.css';
+import api from './services/api';
+import  Main from "./pages/main/index";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
     
     </div>
   );
