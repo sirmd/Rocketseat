9cd2bde2d658dbfee54fac61209a02c38addc63c
Mateus Deitos <mate.deitos@hotmail.com>
2020-05-13 21:16:09 -0300

    NodeJS -> Conectando Database

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
 
 // '/' significa que é o root
 app.get('/', (req, res) => {
-    res.send('Hello MD');
+    res.send('Hello Mateus');
 });
 
 app.listen(3001);
