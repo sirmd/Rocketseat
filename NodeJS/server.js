c22d6eb66af1f78bc0a2f93f5f48d322d08570d9
Mateus Deitos <mate.deitos@hotmail.com>
2020-05-13 21:38:42 -0300

    NodeJS -> Criando model de produto

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// Iniciando o App
 const app = express();
 
+// Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+Product.create({
+    title: 'React Native',
+    description: 'Build apps with React Native',
+    url: 'http://github.com/facebook/react-native'
+});
 
 // '/' significa que é o root
 app.get('/', (req, res) => {
-    res.send('Hello Mateus');
+    return res.send('Hello Mateus');
 });
 
 app.listen(3001);
