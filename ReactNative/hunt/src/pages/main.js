ba14eee3925ac712190c2cbd20d4f70c3a52b9ff
Mateus Deitos <mate.deitos@hotmail.com>
2020-05-29 23:08:24 -0300

    ReactNative -> Buscando Produtos da API

--- ReactNative/hunt/src/pages/main.js
+++ ReactNative/hunt/src/pages/main.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
-import {View,Text} from 'react-native';
+import { View, Text } from 'react-native';
+import api from '../services/api';
 
 export default class Main extends Component {
 
     static navigationOptions = {
         title: "JSHunt"
     };
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products');
+
+        const { docs } = response.data;
+        console.log(docs);
+    }
+
     render() {
-        return(
+        return (
             <View>
                 <Text>Página Main</Text>
             </View>
