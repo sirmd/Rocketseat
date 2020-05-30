ee9438d48b2d8ed5597c135732e44062bd9f9de6
Mateus Deitos <mate.deitos@hotmail.com>
2020-05-29 22:51:13 -0300

    ReactNative -> Estilizando Header e StatusBar

---
+++ ReactNative/hunt/src/config/StatusBarConfig.js
@@ -0,0 +1,5 @@
+import { StatusBar } from 'react-native';
+
+// Configurações para mudar a cor da status bar
+StatusBar.setBackgroundColor('#DA552F'); //android
+StatusBar.setBarStyle('light-content'); //ios
--- ReactNative/hunt/src/routes.js
+++ ReactNative/hunt/src/routes.js
@@ -4,4 +4,17 @@ import Main from './pages/main';
 
 export default createStackNavigator({
     Main,
-});
+},
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#DA552F",
+                
+            },
+            headerTitleStyle:{
+                textAlign:"center",
+                flex: 1
+            },
+            headerTintColor: "#FFF",
+        },
+    });
