be4a0116f6d227b67258e59010e139bdead340ce
Mateus Deitos <mate.deitos@hotmail.com>
2020-06-03 20:02:29 -0300

    NLW -> Aula 3 -> Criado página Home e estilização

--- NextLevelWeek/web/src/App.tsx
+++ NextLevelWeek/web/src/App.tsx
@@ -1,27 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import Header from './Header';
+import Home from './pages/home';
 
 
 function App() {
 
-  // É feito dessa forma para poder atualizar o valor na tela
-  const [counter, setCounter] = useState(0);
+  return (<Home />);
 
-  function handleButtonClick() {
-    //incrementa o counter
-    setCounter(counter + 1);
-
-  }
-
-  return (
-    <div>
-      <Header title={`Contador = ${counter}`} />
-      <h2>{counter}</h2>
-      <h1>Conteúdo da Aplicação</h1>
-      <button type="button" onClick={handleButtonClick}>Incrementa</button>
-    </div>
-  )
 }
 
 export default App;
