5940783c4839687c0f7f36655a6d4832cece0264
Mateus Deitos <mate.deitos@hotmail.com>
2020-06-03 18:18:36 -0300

    NLW -> Aula 3 -> Criado componente Header

--- NextLevelWeek/web/src/App.tsx
+++ NextLevelWeek/web/src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Olá Mundo</h1>
-      </header>
+    <div>
+      <Header/>
+
+      <h1>Conteúdo da Aplicação</h1>
     </div>
-  );
+  )
 }
 
 export default App;
