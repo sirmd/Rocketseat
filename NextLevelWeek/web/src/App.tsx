55f710f29e9ef16b5b297d84e30aa9d337825b94
Mateus Deitos <mate.deitos@hotmail.com>
2020-06-03 19:16:03 -0300

    NLW -> Aula 3 -> Explicação do useState

--- NextLevelWeek/web/src/App.tsx
+++ NextLevelWeek/web/src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Header';
 
+
 function App() {
+
+  // É feito dessa forma para poder atualizar o valor na tela
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    //incrementa o counter
+    setCounter(counter + 1);
+
+  }
+
   return (
     <div>
-      <Header title="Hello World"/>
-
+      <Header title={`Contador = ${counter}`} />
+      <h2>{counter}</h2>
       <h1>Conteúdo da Aplicação</h1>
+      <button type="button" onClick={handleButtonClick}>Incrementa</button>
     </div>
   )
 }
