1ec6d952e1e560a371346f57ac8d7a69eefd6b51
Mateus Deitos <mate.deitos@hotmail.com>
2020-06-03 19:08:08 -0300

    NLW -> Aula 3 -> Criado interface para passar parâmetros ao Header

--- NextLevelWeek/web/src/Header.tsx
+++ NextLevelWeek/web/src/Header.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-function Header() {
+// Interface irá definir os campos do Header
+// campo? = campo não obrigatório
+interface HeaderProps {
+    title?: string
+}
+
+// Para passar parâmetros ao Header é necessário criar a interface e utilizar o React.FC<interface>
+const Header: React.FC<HeaderProps> = (props) => {
     return (
         <header>
-            <h1>Ecoleta</h1>
+            <h1>{props.title}</h1>
         </header>
     );
 }
