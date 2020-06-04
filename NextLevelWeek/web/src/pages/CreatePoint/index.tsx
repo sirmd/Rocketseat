bfee872f4d4186a9b087104a4221ac913d4df0ca
Mateus Deitos <mate.deitos@hotmail.com>
2020-06-03 21:14:59 -0300

    NLW -> Aula 3 -> Buscando todos itens cadastrados da API e exibindo em tela

--- NextLevelWeek/web/src/pages/CreatePoint/index.tsx
+++ NextLevelWeek/web/src/pages/CreatePoint/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../../assets/logo.svg';
 import './styles.css';
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import { Map, TileLayer, Marker } from "react-leaflet";
+import api from "../../services/api";
+
+interface Item {
+    id: number,
+    title: string,
+    image_url: string;
+};
 
 const CreatePoint = () => {
+
+    // Busca items cadastrados na tabela de items
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        })
+    }, []);
+
     return (
         <div id="page-create-point">
             <header>
@@ -78,30 +95,12 @@ const CreatePoint = () => {
                         <span>Selecione um ou mais itens abaixo</span>
                     </legend>
                     <ul className="items-grid">
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/baterias.svg" alt="Teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/papeis-papelao.svg" alt="Teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/eletronicos.svg" alt="Teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/organicos.svg" alt="Teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste" />
-                            <span>Óleo de Cozinha</span>
-                        </li>
+                        {items.map((item) => {
+                            return (<li key={item.id}>
+                                <img src={item.image_url} alt={item.title} />
+                                <span>{item.title}</span>
+                            </li>);
+                        })}
                     </ul>
                 </fieldset>
                 <button type="submit">
